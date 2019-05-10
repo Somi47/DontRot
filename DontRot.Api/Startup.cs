af00f302b65826284b7ad745be13e931280b5cd4
Somi47 <extrackto@gmail.com>
2019-05-10 12:30:08 +0200

    Initial commit

---
+++ DontRot.Api/Startup.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using WebApiLabor.Bll.Services;
+using WebApiLabor.DAL;
+
+namespace WebApiLabor.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddJsonOptions(
+                    json => json.SerializerSettings.ReferenceLoopHandling 
+                            = ReferenceLoopHandling.Ignore);
+
+            services.AddDbContext<NorthwindContext>(o =>
+                o.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"])
+                .ConfigureWarnings(c => c.Throw(RelationalEventId.QueryClientEvaluationWarning)));
+
+            services.AddTransient<IProductService, ProductService>();           
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
---
+++ DontRot.Bll/Dtos/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApiLabor.Api.Dtos
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
